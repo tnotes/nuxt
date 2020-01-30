c23611c88aecfc6ccb331182706060d0cb113f9b
nguyen trung kien <nguyentrungkien@MacBook-Air-cua-nguyen.local>
2020-01-30 08:19:14 +0700

    kien

--- node_modules/sails-mongo/lib/private/constants/expected-url-protocol-pfx.constant.js
+++
@@ -1,5 +0,0 @@
-// EXPECTED_URL_PROTOCOL_PFX
-//
-// The expected prefix for the protocol part of any connection url for this adapter.
-// (or leave `undefined` to attempt to handle this gracefully)
-module.exports = 'mongodb';
